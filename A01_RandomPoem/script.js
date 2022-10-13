57ba537316e16b29362cc1fcd63b0e8eb3e258c5
schaeferle <elias-schaefer@live.de>
2022-10-13 12:32:13 +0200

    Arrays hinzugefügt

--- A01_RandomPoem/script.js
+++ A01_RandomPoem/script.js
@@ -1,2 +1,9 @@
 "use strict";
+var ScriptRandomPoem;
+(function (ScriptRandomPoem) {
+    let subjekteArray = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+    let praedikateArray = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+    let objekteArray = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+    //console.log(subjekteArray, praedikateArray, objekteArray);
+})(ScriptRandomPoem || (ScriptRandomPoem = {}));
 //# sourceMappingURL=script.js.map
---
+++ A01_RandomPoem/script.ts
@@ -0,0 +1,8 @@
+namespace ScriptRandomPoem{
+
+    let subjekteArray : string[] = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"]
+    let praedikateArray: string[] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"]
+    let objekteArray: string[] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"]
+
+    //console.log(subjekteArray, praedikateArray, objekteArray);
+}
