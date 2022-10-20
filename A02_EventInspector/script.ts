474301f855c02adfaea57f1e6562dd6809c3b629
schaeferle <elias-schaefer@live.de>
2022-10-20 18:44:06 +0200

    L02 Upload

---
+++ A02_EventInspector/script.ts
@@ -0,0 +1,7 @@
+/*
+Aufgabe: <L02_EventInspector>
+Name: <Elias Schäfer>
+Matrikel: <268512>
+Datum: <20.10.2022>
+Quellen: <Kommilitonis mit denen Du zusammengearbeitet hast oder von denen Du dich inspirieren ließest>
+*/
