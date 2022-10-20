45994c38c775f04346877155225766925f421bd9
schaeferle <elias-schaefer@live.de>
2022-10-20 18:46:04 +0200

    L02 Upload

---
+++ A02_EventInspector/script.js
@@ -0,0 +1,9 @@
+"use strict";
+/*
+Aufgabe: <L02_EventInspector>
+Name: <Elias Schäfer>
+Matrikel: <268512>
+Datum: <20.10.2022>
+Quellen: <Kommilitonis mit denen Du zusammengearbeitet hast oder von denen Du dich inspirieren ließest>
+*/ 
+//# sourceMappingURL=script.js.map
